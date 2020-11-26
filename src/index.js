e6e0c02354f77bc59b32ee0759f17b3577e8af2a
Hezsel <vhesselp@gmail.com>
2020-11-26 18:51:25 -0300

    chore: cleanup code

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 const routes = require('./routes')
+
 app.use(express.urlencoded({
   extended: true
 }))
